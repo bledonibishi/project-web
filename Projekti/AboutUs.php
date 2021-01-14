2c507c4a03b4a2a86b4847b2d99a8207da46663f
bledonibishi <77398704+bledonibishi@users.noreply.github.com>
2021-01-14 09:53:57 +0100

    Add files via upload

---
+++ Projekti/AboutUs.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="HomePage.css">
+    <title>Document</title>
+</head>
+<body>
+    <section >
+        <div class="about-section">
+        <div class="div1">
+            <h2>Contact us</h2>
+            <h3 class="contact-nr">(038)44-253-127</h3>
+            <h3 class="contact-nr" >(038)49-253-127</h3>
+        </div>
+        <div class="div2">
+            <h2>Mail us</h2>
+            <h3>info@hotmail.com</h3>
+            <h3>info@gmail.com</h3>
+        </div>
+    </div>
+    </section>
+    <h1 class=""></h1>
+</body>
+</html>
---
+++ Projekti/HomePage.php
@@ -0,0 +1,87 @@
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <!-- <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"> -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Open+Sans+Condensed:wght@300&family=Raleway&display=swap" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="HomePage.css"/>
+        <title>Projekti-Front</title>
+    </head>
+    <body>
+        <div class="nav">
+            <a href="" class="nav-item">Home</a>
+            <a href="AboutUs.html" class="nav-item">About us</a>
+            <a href="Register.html" class="nav-item">Register</a>
+            <a href="Login.html" class="nav-item">Login</a>
+        </div>
+
+
+    <section class="main-section">
+        <h1>Learn about nature</h1>
+        <p>All over the land</p>
+    </section>
+    <section class="image">
+         <div >
+            <img class="section-image" src="images/fabian-betto-n9svBYvAOuM-unsplash.jpg">
+        </div>
+    </section>
+    <section class="text">
+        <div class="row">
+            <div class="first-col">
+                <h1>All ABOUT NATURE</h1>
+                <h3>SOME IMPORTANT STUFF ABOUT NATURE</h3>
+            <div class="first-col-header">
+                <p class="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo natus consequuntur alias molestias voluptas quo hic totam pariatur, optio velit odit doloribus accusamus? Excepturi deserunt distinctio modi exercitationem ratione. Officia?</p>
+                <p>This is <em style="color: black;">GrassyGreen</em>, a free, fully standards-compliant CSS template designed by TEMPLATED. The photos in this template are from Fotogrph. This free template is released under the Creative Commons Attribution license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :)
+
+                    Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Duis pretium velit ac mauris. Proin eu wisi suscipit nulla suscipit interdum. Aenean lectus lorem, imperdiet at, ultrices eget, ornare et, wisi. Pellentesque adipiscing purus ac magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna.</p>
+            </div>
+            </div>
+            <div class="second-col">
+                <h1>INTEGER GRAVIDA</h1>
+                <a href="#"class="link1">Amet turpis,feugjat et sit amet</a>
+                <a href="#"class="link1">Ornare in hendrerit in lectust</a>
+                <a href="#"class="link1">Semper mod quis eget mi dolore</a>
+                <a href="#"class="link1">Quam turpis feugiat sit dolor</a>
+                <a href="#"class="link1">Amet ornare in hendrerit in lectus</a>
+                <a href="#"class="link1">Quam turpis feugiat sit dolor</a>
+                <a href="#"class="link1">Consequat etiam lorem phasellus</a>
+            </div>
+        </div>
+    </section>
+    <section class="final-section">
+        <div class="row-end">
+        <div class="first-col-end">
+            <h2>LATEST POST</h2>
+            <a class="a11" href="#">Semper mod quis eget mi dolore</a>
+            <a class="a11" href="#">Quam turpis feugjat sit dolor</a>
+            <a class="a11" href="#">Amet ornare in hendrerit in lectus</a>
+            <a class="a11" href="#">Consequat etiam lorem phasellus</a>
+            <a class="a11" href="#">Amet turpis, feugiat et sit amet</a>
+            <a class="a11" href="#">Semper mod quisturpis nisi</a>
+        </div>
+        <div class="second-col-end">
+            <h2>POPULAR LINKS</h2>
+            <a class="a22" href="#">Semper mod quis eget mi dolore</a>
+            <a class="a22" href="#">Quam turpis feugiat sit dolor</a>
+            <a class="a22" href="#">Amet ornare in hendrerit in lectus</a>
+            <a class="a22" href="#">Consequat etiam lorem phasellus</a>
+            <a class="a22" href="#">Amet turpis, feugiat et sit amet</a>
+            <a class="a22" href="#">Semper mod quisturpis nisi</a>
+        </div>
+        <div class="thirs-col-end">
+            <h2>FOLLOW US</h2>
+            <P>Proin eu wisi suscipit nulla suscipit interdum. Aenean lectus lorem, imperdiet magna.</P>
+            <button class="butoni"><a href="https://en.m.wikipedia.org/wiki/Nature_reserve">Read More</a></button>
+        </div>
+    </div>
+    </section>
+    <section class="section-last">
+        <h2>© UNTITLED. ALL RIGHTS RESERVED. | PHOTOS BY FOTOGRPH |</h2>
+    </section>
+    </body>
+    </html>
